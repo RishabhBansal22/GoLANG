7c93a2fdc1a2cd15af6061bcddfe4041c6ae44f6
RishabhBansal22 <aggrishabh.23@gmail.com>
2025-11-15 15:46:54 +0530

    Implement HTTP GET request and response handling; add server executable

--- go_learn/main.go
+++ go_learn/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world")
+	resp, err := http.Get("https://www.gettyimage.me")
+	if err != nil {
+		// handle error and exit
+		fmt.Println("error fetching:", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		// handle error reading body
+		fmt.Println("error reading response body:", err)
+		return
+	}
+	fmt.Println(string(body))
 }
